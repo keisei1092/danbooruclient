19dd2c39b6c34690bcf26cd1783a465e609935b7
keisei1092 <keiseis1092@gmail.com>
2018-04-30 11:11:10 +0900

    URLを引っ張ってこれる

--- danbooruclient/ViewController.swift
+++ danbooruclient/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var urls: [String] = []
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+		var request = URLRequest(url: URL(string: "https://danbooru.donmai.us/posts.json?tags=hatsune_miku")!)
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
+		URLSession.shared.dataTask(with: request, completionHandler: { [weak self] data, response, error in
+			guard let data = data else { return }
+
+			do {
+				let result = try JSONSerialization.jsonObject(with: data, options: []) as! [AnyObject]
+				self?.urls = result.map { $0["file_url"] as! String }
+				print(result)
+			} catch {
+				print("JSON parse error")
+			}
+		}).resume()
+	}
 
 }
 
